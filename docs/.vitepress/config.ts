d055e02e532125491163f8877945e1cf72471c17
Sourav Bandyopadhyay <souravb.1998@gmail.com>
2024-08-03 23:17:39 +0530

    updated the config.ts file

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,7 +20,15 @@ export default defineConfig({
     [
       "script",
       {},
-      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'GTM-WPJNPGLK');",
+      `(function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WPJNPGLK');`,
     ],
   ],
 
